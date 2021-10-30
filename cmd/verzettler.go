600c0e51482bb06308100d54cc13ac88577f2c35
Patrick Bucher <patrick.bucher@mailbox.org>
2021-10-30 15:44:59 +0200

    use verzettler.go only for module declaration

--- cmd/verzettler.go
+++ cmd/verzettler.go
@@ -1,6 +1,17 @@
 package main
 
-import "github.com/patrickbucher/verzettler"
+import (
+	"github.com/patrickbucher/verzettler/paging"
+	"github.com/patrickbucher/verzettler/rendering"
+	"github.com/signintech/gopdf"
+)
+
+const (
+	rows       = 4
+	cols       = 3
+	outputPath = "example.pdf"
+	fontPath   = "font.ttf"
+)
 
 func main() {
 	words := map[string]string{
@@ -13,5 +24,32 @@ func main() {
 		"Russland":         "Россия",
 		"Junge":            "мальчик",
 	}
-	verzettler.BuildFlashCardsPDF(words, "example.pdf", "font.ttf")
+	BuildFlashCardsPDF(words, outputPath, fontPath)
+}
+
+// BuildFlashCardsPDF takes a map of word pairs, a path to store the resulting
+// PDF, the path to a font file (TTF), and renders a PDF of flash cards.
+func BuildFlashCardsPDF(pairs map[string]string, pdfOutputPath, fontPath string) {
+	pdf := gopdf.GoPdf{}
+	pdf.Start(gopdf.Config{PageSize: *gopdf.PageSizeA4})
+	err := pdf.AddTTFFont("font", fontPath)
+	if err != nil {
+		panic(err)
+	}
+	err = pdf.SetFont("font", "", 14)
+	if err != nil {
+		panic(err)
+	}
+
+	sheets := paging.Distribute(pairs, rows, cols)
+	for _, sheet := range sheets {
+		pdf.AddPage()
+		rendering.DrawGrid(&pdf, sheet.Front)
+		rendering.DistributeWords(&pdf, sheet.Front)
+
+		pdf.AddPage()
+		rendering.DrawGrid(&pdf, sheet.Back)
+		rendering.DistributeWords(&pdf, sheet.Back)
+	}
+	pdf.WritePdf("example.pdf")
 }
